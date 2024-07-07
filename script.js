58d92f4f3b1a912b9c2b8c919ad2aab2cd76ae2e
Yan Kolev <y.kolev11@gmail.com>
2024-07-07 19:07:13 +0200

    Extra OOP theory

--- script.js
+++ script.js
@@ -40,3 +40,45 @@ Polymorphism
 --Polymorphism: A hild class can overwrite a method it inherited from a parent class.
 
 */
+
+/*
+
+
+OOP in Javascript
+
+Clasical OOP -> clases and instances created from those clases.A class is like bluepient, which is theoretical
+plan that we use to build many objects(instances) are instantiated fom a class, which functions like a blueprint. 
+
+In JS things work a little bit differently. 
+
+in JS we have a prototype and all objects in JS are linked to a certain prototype object, 
+
+The prototype object contains methods and propeties that all objects that are linked to that prototype can access and use.
+
+It is called prototypal inheritance. The prototype contains methods that are accessible to all objects linked to that 
+prototype. This inheritance is different that the original pillar of OOP inheritance. 
+
+Behaviou is deledate to the linked prototype object. (its called delegation)
+Objects ---->(delegate)to the prototype.
+
+Example: Array
+
+const num = [1, 2, 3]
+num.map(v => v + 2);
+
+Array.prototype is the prototype of all array objects we create in JS.
+Therefore all arrays have access to the map method.
+
+3 ways of implementing prototypal inheritance in Javascript
+
+1) constructor functions: 
+  -technique to create objects from a function
+  -this is how built-in objects like Arrays, Maps or Sets are actually implemented.
+2) ES6 Classes
+  -Modern alternative to constructor functions syntax
+  -Syntatitc sugar, behind the scenes. ES6 classes work exactly like constructor funtions
+  -ES6 classes DO NOT behave like classes in Classical OOP
+3) Object.create()
+  -Easises most straightforward way of linkng an object to a prototype object.
+
+*/
