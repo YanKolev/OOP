3ccd2009c668a8880062b137328651421353b97e
Yan Kolev <y.kolev11@gmail.com>
2024-07-09 12:33:11 +0200

    Prototypal inheritance on built-in objects

--- script.js
+++ script.js
@@ -169,5 +169,35 @@ console.log(Person.prototype.isPrototypeOf(Person)); // FALSE
 Person.prototype.species = 'Homo Sapiens'
 console.log(jonas.species, matilda.species);
 
-console.log(jonas.hasOwnProperty('firstName'))
-console.log(jonas.hasOwnProperty('species'))
+console.log(jonas.hasOwnProperty('firstName')) // true
+console.log(jonas.hasOwnProperty('species')) // false 
+
+
+
+
+
+/// Prototypal Inheritance on Built-In Objects
+
+console.log(jonas.__proto__);
+//object.prototype(top of prototype chain)
+console.log(jonas.__proto__.__proto__); // up the prototype chain- prototype property of object
+
+console.log(jonas.__proto__.__proto__.__proto__)
+
+console.dir(Person.prototype.constructor);
+
+const arr = [ 3, 6, 4, 6, 3, 4, 5, 6, 9, 3]; //new Array === []
+console.log(arr.__proto__);
+
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function(){
+   return [...new Set(this)]
+}
+
+console.log(arr.unique());
+
+const h1 = document.querySelector ('h1');
+console.dir(x=> x + 1);
