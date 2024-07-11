3bc40a6a69f1e5ab86f4ebb9b570b43b63fab963
Yan Kolev <y.kolev11@gmail.com>
2024-07-11 19:59:36 +0200

    solution update

--- challenge1.js
+++ challenge1.js
@@ -16,3 +16,64 @@ Test data:
 Data car 1: 'BMW' going at 120 km/h
 Data car 2: 'Mercedes' going at 95 km/h
 */
+/* MY TRY
+
+const Car = function (carSpeed, carMake) {
+    this.carSpeed = carSpeed;
+    this.carMake = carMake;
+
+}
+const car1 = new Car (120, 'BMW')
+const car2 = new Car (95, 'Mercedes')
+
+console.log(car1);
+console.log(car2);
+
+//2
+
+Car.prototype.accelerateCar = function(){
+    console.log(this.carSpeed + 10)
+}
+
+car1.accelerateCar();
+car2.accelerateCar();
+
+//3
+Car.prototype.breakOn = function(){
+    console.log(this.carSpeed - 5) //accelerateCar -5??
+}
+car1.breakOn();
+car1.breakOn();
+
+//4
+
+
+//Car.prototype.accelerateCar = accelerateCar();
+//console.log(car1.accelerateCar);
+*/
+
+//Course option 
+
+const Car = function (make, speed) {
+    this.make = make;
+    this.speed = speed;
+};
+
+Car.prototype.acceerate = function(){
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`)
+}
+
+Car.prototype.break = function(){
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`)
+}
+
+const bmw = new Car ('BMW', 120);
+const mercedes = new Car ('Mercedes', 95);
+
+bmw.acceerate();
+bmw.acceerate();
+bmw.break();
+bmw.acceerate();
+
